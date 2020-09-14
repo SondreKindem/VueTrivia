0fa67254057e12f332ca533304329510b541af82
Sondre kindem <sondre.kindem@gmail.com>
2020-09-14 22:01:41 +0200

    chore: display menu first, then gameplay by listening for callback

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,17 +1,40 @@
 <template>
-  <div class="play container">
-    <GameMenu></GameMenu>
+  <div id="play" class="play container mt-6">
+    <GameMenu v-if="!gameStarted" :categories="categories" v-on:play="startGame"></GameMenu>
+    <GamePlay v-if="gameStarted"></GamePlay>
   </div>
 </template>
 
 <script>
 import GameMenu from "@/components/GameMenu";
+import GamePlay from "@/components/GamePlay";
+
 export default {
-name: "Play",
-  components: {GameMenu}
+  name: "Play",
+  components: {GameMenu, GamePlay},
+  data(){
+    return{
+      gameStarted: false,
+      settings: null,
+      categories: []
+    }
+  },
+  methods: {
+    startGame(settings){
+      this.settings = settings;
+      console.log(settings);
+      this.gameStarted = true;
+    }
+  },
+  created() {
+    fetch("https://opentdb.com/api_category.php")
+        .then(response => response.json())
+        .then(data => this.categories = data.trivia_categories);
+  }
 }
 </script>
 
 <style scoped>
-
+#play {
+}
 </style>
