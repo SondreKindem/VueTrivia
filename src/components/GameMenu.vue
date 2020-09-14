95d74d0f4d3414f2e3d706938775a0412a49c572
Sondre kindem <sondre.kindem@gmail.com>
2020-09-14 22:00:45 +0200

    chore: make settings object, and get categories as prop

--- src/components/GameMenu.vue
+++ src/components/GameMenu.vue
@@ -1,40 +1,40 @@
 <template>
-  <div class="container">
+  <div class="container is-vcentered">
     <section>
       <b-field label="Category">
-        <b-select v-model="selectedCategory" placeholder="Any category">
+        <b-select v-model="settings.selectedCategory" placeholder="Any category">
           <option :value="null">Any category</option>
           <option v-for="category in categories" :value="category.id" :key="category.id">{{ category.name }}</option>
         </b-select>
       </b-field>
       <b-field label="Difficulty">
-        <b-select v-model="selectedDifficulty" placeholder="Any difficulty">
+        <b-select v-model="settings.selectedDifficulty" placeholder="Any difficulty">
           <option :value="null">Any difficulty</option>
           <option v-for="difficulty in ['easy', 'medium', 'hard']" :value="difficulty" :key="difficulty">
             {{ difficulty }}
           </option>
         </b-select>
       </b-field>
-      <b-button type="is-primary is-fullwidth">Play!</b-button>
+      <b-button type="is-primary" @click="$emit('play', settings)">Play!</b-button>
     </section>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "GameMenu",
+  props: {
+    categories: Array,
+  },
   data() {
     return {
-      selectedCategory: null,
-      selectedDifficulty: null,
-      categories: [],
+      settings: {
+        selectedCategory: null,
+        selectedDifficulty: null,
+      },
+      gameStarted: false,
     }
-  },
-
-  created() {
-    fetch("https://opentdb.com/api_category.php")
-        .then(response => response.json())
-        .then(data => this.categories = data.trivia_categories);
   }
 }
 </script>
