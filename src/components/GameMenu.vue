5e0b34ba4192f2c1b1f23df12a9d37c9862848e9
Sondre kindem <sondre.kindem@gmail.com>
2020-09-14 11:16:40 +0200

    style: omit function keyword

--- src/components/GameMenu.vue
+++ src/components/GameMenu.vue
@@ -2,11 +2,20 @@
   <div class="container">
     <section>
       <b-field label="Category">
-        <b-select placeholder="Choose category">
-          <option>Any category</option>
-          <option v-for="category in categories" :value="category.id" :key="category.id">{{category.name}}</option>
+        <b-select v-model="selectedCategory" placeholder="Any category">
+          <option :value="null">Any category</option>
+          <option v-for="category in categories" :value="category.id" :key="category.id">{{ category.name }}</option>
         </b-select>
       </b-field>
+      <b-field label="Difficulty">
+        <b-select v-model="selectedDifficulty" placeholder="Any difficulty">
+          <option :value="null">Any difficulty</option>
+          <option v-for="difficulty in ['easy', 'medium', 'hard']" :value="difficulty" :key="difficulty">
+            {{ difficulty }}
+          </option>
+        </b-select>
+      </b-field>
+      <b-button type="is-primary is-fullwidth">Play!</b-button>
     </section>
   </div>
 </template>
@@ -14,17 +23,18 @@
 <script>
 export default {
   name: "GameMenu",
-  data: function () {
+  data() {
     return {
       selectedCategory: null,
+      selectedDifficulty: null,
       categories: [],
     }
   },
 
-  created: function () {
+  created() {
     fetch("https://opentdb.com/api_category.php")
-      .then(response => response.json())
-      .then(data => this.categories = data.trivia_categories);
+        .then(response => response.json())
+        .then(data => this.categories = data.trivia_categories);
   }
 }
 </script>
