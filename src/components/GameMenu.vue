b5a12ba38db510dbfc94cd61551d7ee489b989ce
Sondre kindem <sondre.kindem@gmail.com>
2020-09-15 16:05:29 +0200

    chore: set initial category and difficulty to be 0, this is equal to all

--- src/components/GameMenu.vue
+++ src/components/GameMenu.vue
@@ -3,13 +3,13 @@
     <section>
       <b-field label="Category">
         <b-select v-model="settings.selectedCategory" placeholder="Any category">
-          <option :value="null">Any category</option>
+          <option :value="0">Any category</option>
           <option v-for="category in categories" :value="category.id" :key="category.id">{{ category.name }}</option>
         </b-select>
       </b-field>
       <b-field label="Difficulty">
         <b-select v-model="settings.selectedDifficulty" placeholder="Any difficulty">
-          <option :value="null">Any difficulty</option>
+          <option :value="0">Any difficulty</option>
           <option v-for="difficulty in ['easy', 'medium', 'hard']" :value="difficulty" :key="difficulty">
             {{ difficulty }}
           </option>
@@ -30,12 +30,12 @@ export default {
   data() {
     return {
       settings: {
-        selectedCategory: null,
-        selectedDifficulty: null,
+        selectedCategory: 0,
+        selectedDifficulty: "0",
       },
       gameStarted: false,
     }
-  }
+  },
 }
 </script>
 
