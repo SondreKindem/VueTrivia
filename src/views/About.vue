de33fda2113844aecc6fb10411fa2db48b6b1f6b
Sondre kindem <sondre.kindem@gmail.com>
2020-09-21 11:49:51 +0200

    feat: finish about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-    <p>Does this work?</p>
+    <h1 class="is-size-1">About</h1>
+    <p>A quiz app in Vue.js. Data from <a href="https://opentdb.com">Open Trivia Database</a></p>
+    <p>Source: <a href="https://github.com/SondreKindem/VueTrivia">GitHub</a> | <a href="https://gitlab.com/SondreKindem/triviagame">GitLab</a></p>
   </div>
 </template>
