fa027c29f4b541c264645beb9da106945d471030
Sondre kindem <sondre.kindem@gmail.com>
2020-09-21 09:23:23 +0200

    chore: go to play on click

--- src/components/FrontPage.vue
+++ src/components/FrontPage.vue
@@ -4,14 +4,14 @@
       <h1 class="is-size-1 is-family-code"><b-icon class="has-text-primary" icon="fire" size="is-large"></b-icon><span>Trivia game!</span><b-icon class="has-text-primary" icon="fire" size="is-large"></b-icon></h1>
     </div>
     <div class="section">
-      <b-button class="is-primary is-large is-family-code">Play the game!</b-button>
+      <b-button class="is-primary is-large is-family-code" @click="$router.push('/play')">Play the game!</b-button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-name: "FrontPage"
+  name: "FrontPage"
 }
 </script>
 
